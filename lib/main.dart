97990ff34def5f56afe24ce9e1134bf057459a9d
daneelsa <daneelsaa@gmail.com>
2026-03-19 17:59:29 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProfileScreen(),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // The Scaffold is our "Canvas"
+    return Scaffold(
+      backgroundColor: const Color(0xFFDDE5E9), // The grey-blue background
+      
+    );
+  }
+}
